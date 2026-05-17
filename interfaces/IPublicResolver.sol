91b5695b36b52fd021354d010cce0c55510c1841
JR-me <eafe15@gmail.com>
2026-05-17 12:49:37 +0000

    Add files via upload

--- interfaces/IPublicResolver.sol
+++ interfaces/IPublicResolver.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.20;
 
 interface IPublicResolver {
-    function setAddr(bytes32 node, address addr) external;
+    function setAddr(bytes32 node, address _addr) external;
     function setText(bytes32 node, string calldata key, string calldata value) external;
     function setContenthash(bytes32 node, bytes calldata hash) external;
     function addr(bytes32 node) external view returns (address payable);
