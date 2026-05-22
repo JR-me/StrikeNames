d51233f641994c1ced764eb7f4f67681fa9cd32f
JR-me <eafe15@gmail.com>
2026-05-22 03:25:54 +0000

    Update layout.tsx

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -11,6 +11,14 @@ import '@rainbow-me/rainbowkit/styles.css'
 
 const queryClient = new QueryClient()
 
+export const metadata: Metadata = {
+  // ── Add this ──
+  other: {
+    'talentapp:project_verification':
+      '1e52dc112b874d2040736a6266e35ecacb2ea3744c671ced5b640490948ac82063f28f41fae3fba8f883631f3a61a9c6eaa68370e03c1ab65897eb36f1538848',
+  },
+}
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
